d39a41329e37364aba1b47550a9248fcb56b63eb
Serhii Rachuk <0432502095x@gmail.com>
2025-01-19 00:19:02 +0000

    test commit

--- joblist/urls.py
+++ joblist/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   # path('job/<int:job_id>/', views.detail, name='detail'),
-   # path('category/<str:category_name>/', views.joblist, name='joblist_by_category'),
-   # path('', views.joblist, name='joblist'),  # Додано цей шлях для /joblist/
+   path('job/<int:job_id>/', views.detail, name='detail'),
+   path('category/<str:category_name>/', views.joblist, name='joblist_by_category'),
+   path('', views.joblist, name='joblist'),  # Додано цей шлях для /joblist/
 ]
