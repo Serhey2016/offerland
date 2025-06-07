255b99216b0a2521868723175ea84c42706fd1a0
Serhii <0432502095x@gmail.com>
2025-06-07 11:49:10 +0000

    implemented allauth library

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.29', 'localhost', '127.0.0.1', '0.0.0.0', '192.168.0.146']
+ALLOWED_HOSTS = ['192.168.0.29', 'localhost', '127.0.0.1', '0.0.0.0', '192.168.0.146', '192.168.0.226']
 
 
 # Application definition
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
